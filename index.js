eaa04ac45205f846c7abe39a488c2b7b06be9eb7
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-07-03 19:49:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * IMPORTANT NOTICE:
+ * 
+ * The data is provided by the data.js file.
+ * Make sure the data.js file is loaded before the index.js file, so that you can acces it here!
+ * The data is provided in an array called: data
+ */
